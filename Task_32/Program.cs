d88780a65a6c0a9572a1805935b4e3399aa899ea
ELENA <battilena@gmail.com>
2022-09-05 17:49:39 +0300

    Task 32

---
+++ Task_32/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу замена элементов массива: положительные элементы замените 
+//на соответствующие отрицательные, и наоборот
+Console.Clear();
+Console.WriteLine("Укажите длину массива: ");
+int size = int.Parse(Console.ReadLine());
+int [] rand = new int[size];
+int [] rand2 = new int [size];
+for (int i = 0; i < rand.Length; i++) {
+    rand[i] = new Random{}.Next(-99, 100);
+    Console.Write(rand[i]+" ");
+}
+Console.WriteLine();
+for (int i = 0; i < rand.Length; i++){
+    rand2 [i] =rand [i] * -1;
+    Console.Write(rand2[i]+" ");
+}
+Console.WriteLine();
