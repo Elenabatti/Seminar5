72f65cc41e87a1d5917f3aa77055c32eea8b1bb5
ELENA <battilena@gmail.com>
2022-09-05 20:44:44 +0300

    + comments

--- Task_32/Program.cs
+++ Task_32/Program.cs
@@ -2,16 +2,18 @@
 //на соответствующие отрицательные, и наоборот
 Console.Clear();
 Console.WriteLine("Укажите длину массива: ");
-int size = int.Parse(Console.ReadLine());
-int [] rand = new int[size];
+
+int size = int.Parse(Console.ReadLine());// значение size длина массива
+int [] rand = new int[size]; //создаем сразу 2 массива одинаковой размерности, rand и  rand2
 int [] rand2 = new int [size];
-for (int i = 0; i < rand.Length; i++) {
+for (int i = 0; i < rand.Length; i++) {//  забиваем наш массив длиной rand  элементами от -99 до 100
     rand[i] = new Random{}.Next(-99, 100);
     Console.Write(rand[i]+" ");
 }
 Console.WriteLine();
-for (int i = 0; i < rand.Length; i++){
-    rand2 [i] =rand [i] * -1;
+for (int i = 0; i < rand.Length; i++){// прогоняем тот же самый  for  по первому массиву и 
+    rand2 [i] =rand [i] * -1;// coздаем 2й массив и прогоняем все 
+    //  берем элемент с таким же номером у первого массива, соответствующие элементам 2го умножаем на -1 
     Console.Write(rand2[i]+" ");
 }
 Console.WriteLine();
