0d7860f3af89b6cb40c73c1dd180b60caf6438c3
ELENA <battilena@gmail.com>
2022-09-06 12:16:33 +0300

    task33(one line)

---
+++ Task33_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте массив и напишите программу, которая определяет присутствует ли заданное число в массиве
+Console.Clear();
+Console.WriteLine("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int [] array = GetArray(size, -9, 9);
+
+Console.WriteLine(String.Join(" ", array));
+
+Console.WriteLine("Введите значение");
+int findNumber = int.Parse(Console.ReadLine());
+Console.WriteLine(array.FirstOrDefault(p=>p == findNumber) ! = 0 ? "Да" : "Нет");
+
+int [] GetArray(int size, int min, int max){
+    int[] result = new int [size];
+
+    for (int i = 0; i < size; i++){
+result[i] = new Random() .Next(min, max +1);
+    }
+}
